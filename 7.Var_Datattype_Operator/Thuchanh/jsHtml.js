f237cf7b64947eca330552d8e449edecc29c4dbf
C1020G1_BảoThịnh <baothinhtran159@gmail.com>
2020-11-04 13:48:02 +0700

    a

---
+++ 7.Var_Datattype_Operator/Thuchanh/jsHtml.js
@@ -0,0 +1,11 @@
+let inputwidth ;
+let inputheight ;
+
+inputwidth = prompt("Enter width") ;
+inputheight = prompt("Enter height");
+
+let width = parseInt(inputwidth) ;
+let height = parseInt(inputheight) ;
+
+let area = width*height ;
+document.write("The area is:" + area) ;
