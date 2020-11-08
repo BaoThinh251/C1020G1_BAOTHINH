c5434a3d7031823b32b96cd5d5d8d09dc6986179
C1020G1_BảoThịnh <baothinhtran159@gmail.com>
2020-11-08 22:27:13 +0700

    b

--- CaseStudy/task2-1.js
+++ CaseStudy/task2-1.js
@@ -1,8 +1,22 @@
 let dis = document.getElementById("discount");
 let rent = document.getElementById("rentdays");
 let typeroom = document.getElementById("typeofroom");
+let id = document.getElementById("I");
+let age = document.getElementById("birth");
+let mail = document.getElementById("email");
+let addr = document.getElementById("address");
+let cusrank = document.getElementById("cusrank");
+let roomgrade = document.getElementById("roomgrade");
+let addcus = document.getElementById("+cus");
 let result;
 function submit() {
     result = (1-parseInt(dis.value)/100) * (parseInt(rent.value)) * (parseInt(typeroom.value));
-    alert(result + '$');
+    document.getElementById("cost").innerHTML = result;
+    document.getElementById("ID").innerHTML = id.value;
+    document.getElementById("bday").innerHTML = age.value;
+    document.getElementById("mail").innerHTML = mail.value;
+    document.getElementById("addr").innerHTML = addr.value;
+    document.getElementById("cusranking").innerHTML = cusrank.value;
+    document.getElementById("roomgrad").innerHTML = roomgrade.value;
+    document.getElementById("+cust").innerHTML = addcus.value;
 }
